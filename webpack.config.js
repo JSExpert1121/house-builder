9bb49357ada884a664918d051d7c1c8be8358523
JSExpert1121 <jingang0314@gmail.com>
2019-04-07 02:09:00 +0800

    configured eslint,

--- webpack.config.js
+++ webpack.config.js
@@ -3,59 +3,62 @@ var path = require('path');
 const Dotenv = require('dotenv-webpack')
 const webpack = require('webpack');
 
-module.exports = env=>{
+module.exports = env => {
 
-    const isProduction = env && env.production !== undefined
+	const isProduction = env && env.production !== undefined
 
-const plugins = [
-    new Dotenv(),
-    new webpack.HotModuleReplacementPlugin()
-]
+	const plugins = [
+		new Dotenv(),
+		new webpack.HotModuleReplacementPlugin()
+	]
 
-    return {
-    entry: './src/app.js',
-    mode:isProduction?'production':'development',
-    output: {
-      path: path.resolve(__dirname, 'public'),
-      filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
-        }, {
-        test: /\.scss$/,
-        use: [
-        'style-loader',
-        'css-loader', //translates css into commonJS
-        'sass-loader' // compile Sass to CSS using node sass by default
-        ]
-        }]
-        },
+	return {
+		entry: './src/app.js',
+		mode: isProduction ? 'production' : 'development',
+		output: {
+			path: path.resolve(__dirname, 'public'),
+			filename: 'bundle.js'
+		},
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					use: ['eslint-loader', 'babel-loader']
+				},
+				{
+					test: /\.scss$/,
+					use: [
+						'style-loader',
+						'css-loader', //translates css into commonJS
+						'sass-loader' // compile Sass to CSS using node sass by default
+					]
+				}
+			]
+		},
 
-        devtool:  isProduction?'none':'cheap-module-eval-source-map',
+		devtool: isProduction ? 'none' : 'cheap-module-eval-source-map',
 
-        devServer: {
-            contentBase: path.join(__dirname, 'public'),
-            historyApiFallback: true, // this prevents the default browser full page refresh on form submission and link change
-            port:8000,
-            hot: true, //enable hot module replacement feature
-            },
-            plugins:plugins,
-            resolve : {
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true, // this prevents the default browser full page refresh on form submission and link change
+			port: 8000,
+			hot: true, //enable hot module replacement feature
+		},
+		plugins: plugins,
+		resolve: {
 
-                extensions:['.js'],
-                alias:{
+			extensions: ['.js'],
+			alias: {
 
-                    containers: path.resolve(__dirname,'src/containers'),
-                    config: path.resolve(__dirname,'src/config'),
-                    routers:path.resolve(__dirname,'src/routers'),
-                    components:path.resolve(__dirname,'src/components')
-                }
-            }
-    }
-    
+				containers: path.resolve(__dirname, 'src/containers'),
+				config: path.resolve(__dirname, 'src/config'),
+				routers: path.resolve(__dirname, 'src/routers'),
+				components: path.resolve(__dirname, 'src/components')
+			}
+		}
+	}
 
-   
-  }
+
+
+}
