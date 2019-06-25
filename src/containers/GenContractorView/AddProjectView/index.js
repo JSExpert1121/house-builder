a66794cebcb55be9d21e5aa39bdaa3b9126783b1
JSExpert1121 <jingang0314@gmail.com>
2019-06-25 11:57:30 +0700

    [ref#theme] :arrow_upper_right: Using pro theme

--- src/containers/GenContractorView/AddProjectView/index.js
+++ src/containers/GenContractorView/AddProjectView/index.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
-import { withRouter }       from 'react-router-dom';
-import { connect }          from 'react-redux';
-import PropTypes            from 'prop-types';
-// material ui
-import { withStyles }       from '@material-ui/core/styles';
-import Paper                from '@material-ui/core/Paper';
-import CircularProgress     from '@material-ui/core/CircularProgress';
-import Card                 from '@material-ui/core/Card';
-import TextField            from '@material-ui/core/TextField';
-import Button               from '@material-ui/core/Button';
-import IconButton           from '@material-ui/core/IconButton';
-import Box                  from '@material-ui/core/Box';
+import React, {Component}         from 'react';
+import {withRouter}               from 'react-router-dom';
+import {connect}                  from 'react-redux';
+import {compose}                  from "redux";
+import {createStyles, withStyles} from '@material-ui/core/styles';
+import Paper                      from '@material-ui/core/Paper';
+import CircularProgress           from '@material-ui/core/CircularProgress';
+import Card                       from '@material-ui/core/Card';
+import Button                     from '@material-ui/core/Button';
+import IconButton                 from '@material-ui/core/IconButton';
+import Box                        from '@material-ui/core/Box';
+import CloudUploadIcon            from '@material-ui/icons/CloudUpload';
+import DeleteIcon                 from '@material-ui/icons/Delete';
 
-import CloudUploadIcon from '@material-ui/icons/CloudUpload';
-import DeleteIcon      from '@material-ui/icons/Delete';
-
-import SimpleMDE from 'react-simplemde-editor';
+import CustomInput from "components/CustomInput/CustomInput.jsx";
+import SimpleMDE   from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 
-import { addFilesToProject, addProject } from '../../../actions/global-actions';
-import CustomSnackbar                    from '../../../components/shared/CustomSnackbar';
+import {addFilesToProject, addProject} from '../../../actions/global-actions';
+import CustomSnackbar                  from '../../../components/shared/CustomSnackbar';
 
-const styles = theme => ({
+const styles = theme => createStyles({
   root: {
     position: 'relative',
     flexGrow: 1,
     height: 'calc(100vh - 128px)',
-    margin: theme.spacing(1),
+    marginTop: theme.spacing(1),
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -36,9 +34,6 @@ const styles = theme => ({
     height: '100%',
     borderBottom: '5px solid ' + theme.palette.primary.light,
     padding: theme.spacing(2),
-    // [theme.breakpoints.up('sm')]: {
-    // 	width: 700,
-    // },
     display: 'flex',
     flexDirection: 'column',
     overflow: 'auto',
@@ -88,7 +83,7 @@ const styles = theme => ({
   },
 });
 
-class connectedAddProjectView extends Component {
+class AddProjectView extends Component {
   constructor(props) {
     super(props);
 
@@ -171,20 +166,28 @@ class connectedAddProjectView extends Component {
     return (
       <Paper className={classes.root}>
         <Card className={classes.mainBoard}>
-          <TextField
-            label="Project Title"
-            className={classes.paper_title}
-            value={this.state.title}
-            onChange={val => this.setState({ title: val.target.value })}
-            InputProps={{ classes: { input: classes.editField } }}
+          <CustomInput
+            labelText="Project Title"
+            id="title"
+            formControlProps={{
+              fullWidth: true
+            }}
+            inputProps={{
+              value: this.state.title,
+              onChange: val => this.setState({ title: val.target.value })
+            }}
           />
-          <TextField
-            type="number"
-            label="Price"
-            className={classes.paper_price}
-            value={this.state.price}
-            onChange={val => this.setState({ price: val.target.value })}
-            InputProps={{ classes: { input: classes.editField } }}
+          <CustomInput
+            labelText="Price"
+            id="price"
+            formControlProps={{
+              fullWidth: true
+            }}
+            inputProps={{
+              type: "number",
+              value: this.state.price,
+              onChange: val => this.setState({ price: val.target.value })
+            }}
           />
           <SimpleMDE
             style={{ height: 'calc(100% - 274px)', overflow: 'auto' }}
@@ -248,27 +251,20 @@ class connectedAddProjectView extends Component {
   }
 }
 
-const mapDispatchToProps = dispatch => {
-  return {
-    addProject: (id, data) => dispatch(addProject(id, data)),
-    addFiles: (id, files) => dispatch(addFilesToProject(id, files)),
-  };
+const mapDispatchToProps = {
+  addProject,
+  addFiles: addFilesToProject,
 };
 
-const mapStateToProps = state => {
-  return {
-    userProfile: state.global_data.userProfile,
-  };
-};
+const mapStateToProps = state => ({
+  userProfile: state.global_data.userProfile,
+});
 
-const AddProjectView = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(connectedAddProjectView);
-
-connectedAddProjectView.propTypes = {
-  classes: PropTypes.object.isRequired,
-  userProfile: PropTypes.object.isRequired,
-};
-
-export default withRouter(withStyles(styles)(AddProjectView));
+export default compose(
+  withRouter,
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  ),
+  withStyles(styles),
+)(AddProjectView);
